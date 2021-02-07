b7b7ab52b56da3ebe9277cce23bc9732fffa0a8e
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2021-02-07 17:42:24 -0300

    feat: add basic template

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <img src="https://avatars.githubusercontent.com/u/9921030?v=4" alt="avatar" />
+      <h2>
+        <a href="https://api.github.com/users/guicazaroto">Guilherme Cazaroto</a>
+      </h2>
+      <div className="followers">
+        <h3>Relations</h3>
+        <ul>
+          <li>followers: 100</li>
+          <li>following: 3</li>
+        </ul>
+      </div>
+
+      <div className="followers">
+        <h3>Relations</h3>
+        <ul>
+          <li>followers: 100</li>
+          <li>following: 3</li>
+        </ul>
+      </div>
+
+      <div className="repositories">
+        <h3>Repositories</h3>
+      </div>
+
+      <div className="starred">
+        <h3>Starred</h3>
+      </div>
     </div>
   );
 }
